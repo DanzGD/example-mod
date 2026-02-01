c6fdd8281c639e1ed7205abe0c018b99bedc8761
DanzGD <nasyit2013@gmail.com>
2026-02-01 11:11:28 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,59 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/EditorUI.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+// Kita memodifikasi "EditorUI" (Tampilan antarmuka Editor)
+class $modify(MyEditorUI, EditorUI) {
+    
+    // Fungsi init berjalan saat kita masuk ke Editor
+    bool init(LevelEditorLayer* editorLayer) {
+        // Jalankan kode asli game dulu agar editor muncul normal
+        if (!EditorUI::init(editorLayer)) {
+            return false;
+        }
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+        // 1. Buat Menu (Wadah untuk tombol)
+        auto myMenu = CCMenu::create();
+        
+        // Atur posisi menu (X=30 di kiri, Y=tinggi layar - 50 pixel)
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+        myMenu->setPosition({30, winSize.height - 50});
+        
+        // Beri ID agar kompatibel dengan mod lain
+        myMenu->setID("quick-save-menu"_spr);
+        
+        // Masukkan menu ke layar editor
+        this->addChild(myMenu);
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+        // 2. Buat Gambar Tombol (Menggunakan gambar Save bawaan game)
+        auto sprite = CCSprite::createWithSpriteFrameName("GJ_saveBtn_001.png");
+        sprite->setScale(0.6f); // Kecilkan sedikit agar tidak menghalangi
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+        // 3. Buat Tombolnya dan hubungkan ke fungsi "onQuickSave"
+        auto btn = CCMenuItemSpriteExtra::create(
+            sprite,
+            this,
+            menu_selector(MyEditorUI::onQuickSave)
+        );
+        
+        // Masukkan tombol ke dalam menu
+        myMenu->addChild(btn);
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+        return true;
+    }
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+    // Fungsi yang jalan saat tombol dipencet
+    void onQuickSave(CCObject* sender) {
+        // Memanggil fungsi asli "onSave" milik EditorUI
+        this->onSave(nullptr);
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
-
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+        // Opsional: Munculkan notifikasi kecil (NotificationToast)
+        // Agar kita tahu save berhasil tanpa popup yang mengganggu
+        Notification::create(
+            "Level Saved!", 
+            NotificationIcon::Success, 
+            0.5f // Durasi notifikasi
+        )->show();
+    }
 };
